e095270b32656754db8dc5e1a99c49390405d4e0
aminevg <38422328+aminevg@users.noreply.github.com>
2025-02-12 23:00:26 +0900

    commit progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    server: { port: 9999, },
-    integrations: [tailwind(), react()],
+    server: { port: 9999 },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [react()],
 });
